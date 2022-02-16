e6c3f4131cf69f2c2dfd7935cc6723b985c33c8c
Deonte Cooper <deonte.cooper12@gmail.com>
2022-02-15 21:37:31 -0700

    Add routes '/prospects_files/import' and '/prospects_files/:id/progress'

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-    resources :warmup, only: [:index]
+  resources :warmup, only: [:index]
 
-    namespace :api do
-        resources :users, only: [:create]
-        post "/login", to: "users#login"
-        get "/user", to: "users#user"
-        get "/campaigns", to: "campaigns#show"
-        get "/campaigns/search", to: "campaigns#search"
-        post "/campaigns", to: "campaigns#create"
-        post "/campaigns/:id/prospects", to: "campaigns#add_prospects"
-        get "/prospects", to: "prospects#show"
-        post "/prospects", to: "prospects#create"
-    end
+  namespace :api do
+      resources :users, only: [:create]
+      post "/login", to: "users#login"
+      get "/user", to: "users#user"
+      get "/campaigns", to: "campaigns#show"
+      get "/campaigns/search", to: "campaigns#search"
+      post "/campaigns", to: "campaigns#create"
+      post "/campaigns/:id/prospects", to: "campaigns#add_prospects"
+      get "/prospects", to: "prospects#show"
+      post "/prospects", to: "prospects#create"
+      post "/prospects_files/import", to: "prospects_files#import"
+      get "/prospects_files/:id/progress", to: "prospects_files#progress"
+  end
 end
