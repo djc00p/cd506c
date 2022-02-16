373b22245ba2aa274207b98ff8a6411af502e62e
Deonte Cooper <deonte.cooper12@gmail.com>
2022-02-15 21:28:49 -0700

    Update schema with new migrations

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_145653) do
+ActiveRecord::Schema.define(version: 2022_02_09_144137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "campaigns", force: :cascade do |t|
     t.text "name"
@@ -40,6 +68,19 @@ ActiveRecord::Schema.define(version: 2021_11_22_145653) do
     t.index ["user_id"], name: "index_prospects_on_user_id"
   end
 
+  create_table "prospects_files", force: :cascade do |t|
+    t.string "file"
+    t.integer "email_index"
+    t.integer "first_name_index?"
+    t.integer "last_name_index?"
+    t.boolean "force?"
+    t.boolean "has_headers?"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_prospects_files_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -48,6 +89,9 @@ ActiveRecord::Schema.define(version: 2021_11_22_145653) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "campaigns", "users"
   add_foreign_key "prospects", "users"
+  add_foreign_key "prospects_files", "users"
 end
