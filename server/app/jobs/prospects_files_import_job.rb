93e5d7f7a59ade9b2f7a5fbd1fa96781011a4609
Deonte Cooper <deonte.cooper12@gmail.com>
2022-02-19 11:33:49 -0700

    Remove email comments and Add descriptive variable names

--- server/app/jobs/prospects_files_import_job.rb
+++ server/app/jobs/prospects_files_import_job.rb
@@ -5,19 +5,17 @@ class ProspectsFilesImportJob < ApplicationJob
   retry_on ActiveRecord::Deadlocked, wait: 5.seconds, attempts: 3
   retry_on Net::OpenTimeout,
            Timeout::Error, wait: :exponentially_longer, attempts: 10
-           
+
   queue_as :default
 
   around_perform do |job, block|
-    # Send an email to user that the file being processed
     block.call
-    # Send an email to user that the file has been processed
   end
 
-  def perform(pf_import)
-    pf_data = pf_import.file.download
+  def perform(prospect_file)
+    prospect_file_data = prospect_file.file.download
 
-    pf_import.csv_import(pf_data)
-    delete_tmp(tmp_file_path(pf_data))
+    prospect_file.csv_import(prospect_file_data)
+    delete_tmp(tmp_file_path(prospect_file_data))
   end
 end
