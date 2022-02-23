20420f635544d480381151572ea3a0439d65371c
Deonte Cooper <deonte.cooper12@gmail.com>
2022-02-23 13:17:41 -0700

    Update ProspectsFilesController progress and file_length_validation methods

--- server/app/controllers/api/prospects_files_controller.rb
+++ server/app/controllers/api/prospects_files_controller.rb
@@ -18,10 +18,10 @@ class Api::ProspectsFilesController < ApplicationController
 
   def progress
     prospect_file = ProspectsFiles.find_by(id: params[:id], user_id: @user.id)
-    total = prospect_file.row_count
-    done =  prospect_file.done
-
     if prospect_file.present?
+      total = prospect_file.total_rows
+      done =  Prospect.where(user_id: @user.id, prospects_file_id: prospect_file.id).count
+      
       render json: { total: total, done: done }
     else
       render json: { error: prospect_file.errors.full_messages }, status: :not_found
@@ -31,14 +31,16 @@ class Api::ProspectsFilesController < ApplicationController
   private
 
   def prospects_files_params
-    params.permit(:file, :email_index, :first_name_index?,:last_name_index?, :force?, :has_headers?)
+    params.permit(:file, :email_index, :first_name_index?,:last_name_index?, :force?, :has_headers?, :total_rows)
   end
 
   def file_length_validation
     has_headers = ActiveRecord::Type::Boolean.new.cast(params[:has_headers?])
     file_length = CSV.read(params[:file], headers: has_headers).count
     if file_length > 1000000
-      errors[:file] << "should be less than 1,000,000 rows"
+      errors[:file] << "should be less than 1000000 rows"
+    else
+      params[:total_rows] = file_length
     end
   end
 end
