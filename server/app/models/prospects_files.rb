d3e1729233e92387f4edb97e74987d73719cfc65
Deonte Cooper <deonte.cooper12@gmail.com>
2022-02-23 13:19:20 -0700

    Update ProspectFiles methods

--- server/app/models/prospects_files.rb
+++ server/app/models/prospects_files.rb
@@ -1,6 +1,4 @@
 class ProspectsFiles < ApplicationRecord
-  include TmpImportFile # lib/modules/tmp_import_file.rb
-
   has_one_attached :file
   belongs_to :user
   has_many :prospects
@@ -10,56 +8,46 @@ class ProspectsFiles < ApplicationRecord
             content_type: { in: 'text/csv', message: 'Not a CSV File' },
             size: { less_than_or_equal_to: 200.megabytes, message: 'file to big'}
 
-  def csv_import(pf_data)
-    CSV.foreach(tmp_file_path(pf_data), headers: self.has_headers?) do |row|
+  def csv_import(prospect_file)
+    prospect_file.blob.open(tmpdir: Rails.root.join('tmp')) do |tmp_file|
+      parse_tmp_csv_file(tmp_file)
+    end
+  end
+
+  def parse_tmp_csv_file(file)
+    CSV.foreach(file, headers: self.has_headers?) do |row|
       email = headers?(row.to_a[self.email_index])
       first_name = headers?(row.to_a[self.first_name_index?])
       last_name = headers?(row.to_a[self.last_name_index?])
       user_id = self.user_id
+      prospects_file_id = self.id
 
-      force_check(self.force?, email, first_name, last_name, user_id)
+      create_or_update_prospect(self.force?, email, first_name, last_name, user_id, prospects_file_id)
     end
   end
 
-  def force_check(force, email, first_name, last_name, user_id)
-    if force
-      force(email, first_name, last_name, user_id)
+  def create_or_update_prospect(force, email, first_name, last_name, user_id, prospects_file_id)
+    if force && Prospect.exists?(email: email, user_id: user_id)
+      update_prospect(email, first_name, last_name, user_id, prospects_file_id)
+    elsif force == false && Prospect.exists?(email: email, user_id: user_id)
     else
-      create_prospect(email, first_name, last_name, user_id)
+      create_prospect(email, first_name, last_name, user_id, prospects_file_id)
     end
   end
 
-  def force(email, first_name, last_name, user_id)
-    if Prospect.exists?(email: email)
-      update_prospect(email, first_name, last_name, user_id)
+  def create_prospect(email, first_name, last_name, user_id, prospects_file_id)
+    Prospect.create!(email: email, first_name: first_name, last_name: last_name, user_id: user_id, prospects_file_id: prospects_file_id)
+  end
+
+  def update_prospect(email, first_name, last_name, user_id, prospects_file_id)
+    Prospect.update_all(email: email, first_name: first_name, last_name: last_name, user_id: user_id, prospects_file_id: prospects_file_id)
+  end
+
+  def headers?(attribute)
+    if attribute.kind_of?(Array)
+      attribute[1]
     else
-      create_prospect(email, first_name, last_name, user_id)
-    end
-  end
-
-  def create_prospect(email, first_name, last_name, user_id)
-    Prospect.create!(
-      email: email,
-      first_name: first_name,
-      last_name: last_name,
-      user_id: user_id
-    )
-  end
-
-  def update_prospect(email, first_name, last_name, user_id)
-    Prospect.update_all(
-      email: email,
-      first_name: first_name,
-      last_name: last_name,
-      user_id: user_id
-    )
-  end
-
-  def headers?(a)
-    if a.kind_of?(Array)
-      a[1]
-    else
-      a
+      attribute
     end
   end
 end
