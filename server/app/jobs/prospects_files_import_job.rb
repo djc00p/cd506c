80cf328b0ba5c6bf9cd85180c3d105332fac6ef0
Deonte Cooper <deonte.cooper12@gmail.com>
2022-02-23 13:11:39 -0700

    Update ProspectsFilesImportJob perform method

--- server/app/jobs/prospects_files_import_job.rb
+++ server/app/jobs/prospects_files_import_job.rb
@@ -1,8 +1,6 @@
 require 'csv'
 
 class ProspectsFilesImportJob < ApplicationJob
-  include TmpImportFile # lib/modules/tmp_import_file.rb
-  retry_on ActiveRecord::Deadlocked, wait: 5.seconds, attempts: 3
   retry_on Net::OpenTimeout,
            Timeout::Error, wait: :exponentially_longer, attempts: 10
 
@@ -13,9 +11,8 @@ class ProspectsFilesImportJob < ApplicationJob
   end
 
   def perform(prospect_file)
-    prospect_file_data = prospect_file.file.download
+    prospect_file_attachment = prospect_file.file
 
-    prospect_file.csv_import(prospect_file_data)
-    delete_tmp(tmp_file_path(prospect_file_data))
+    prospect_file.csv_import(prospect_file_attachment)
   end
 end
